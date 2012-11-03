b010d11a4d6f6acbe2d695852b1420f85501fcdd
Maksymilian Owsianny <Maksymilian.Owsianny@gmail.com>
2012-11-03 18:33:33 +0100

    History documentation.

--- src/Graphics/UI/Awesomium/History.hs
+++ src/Graphics/UI/Awesomium/History.hs
@@ -12,27 +12,45 @@ module Graphics.UI.Awesomium.History
 
 import Graphics.UI.Awesomium.Raw
 
+import Foreign.Ptr
+
+-- | Destroy the instance (you must call this once you're done using
+-- the instance)
 queryResultDestroy :: HistoryQueryResult -> IO ()
 queryResultDestroy = awe_history_query_result_destroy
 
+-- | Get the total number of entries
 queryResultGetSize :: HistoryQueryResult -> IO Int
 queryResultGetSize = awe_history_query_result_get_size
 
-queryResultGetEntryAtIndex :: HistoryQueryResult -> Int -> IO HistoryEntry
-queryResultGetEntryAtIndex = awe_history_query_result_get_entry_at_index
+-- | Get a certain entry (you must destroy any returned entry using
+-- 'entryDestroy'). May return Nothing if the index is out of
+-- bounds.
+queryResultGetEntryAtIndex :: HistoryQueryResult -> Int
+                           -> IO (Maybe HistoryEntry)
+queryResultGetEntryAtIndex i r =
+    awe_history_query_result_get_entry_at_index i r >>= \res ->
+    if (res == nullPtr)
+        then return Nothing
+        else return $ Just res
 
+-- | Destroy the instance
 entryDestroy :: HistoryEntry -> IO ()
 entryDestroy = awe_history_entry_destroy
 
+-- | Get the URL of the page
 entryGetUrl :: HistoryEntry -> IO String
 entryGetUrl = awe_history_entry_get_url
 
+-- | Get the title of the page
 entryGetTitle :: HistoryEntry -> IO String
 entryGetTitle = awe_history_entry_get_title
 
+-- | Get the last time this page was visited (in seconds since epoch)
 entryGetVisitTime :: HistoryEntry -> IO Double
 entryGetVisitTime = awe_history_entry_get_visit_time
 
+-- | Get the number of times this page was visited.
 entryGetVisitCount :: HistoryEntry -> IO Int
 entryGetVisitCount = awe_history_entry_get_visit_count
 
